e19e551ecb3a9a04b849cc0cbbbca4f61f78b4a2
Daniel Yao <dyao13@jh.edu>
2025-02-19 00:31:50 -0500

    initial commit

---
+++ code/mcmc.py
@@ -0,0 +1,159 @@
+import os
+
+import ngrams
+import random
+import math
+from tqdm import tqdm
+from collections import Counter
+import string
+
+def inv_perm(perm):
+    """
+    Inverts a permutation.
+
+    Parameters:
+        perm (dict): The permutation to invert.
+
+    Returns:
+        dict: The inverted permutation.
+    """
+    return {v: k for k, v in perm.items()}
+
+def energy_function(perm, scrambled_text, P, Q):
+    """
+    Computes the energy of a permutation given a scrambled text and n-gram probabilities.
+
+    Parameters:
+        perm (dict): The permutation to evaluate.
+        scrambled_text (str): The scrambled text.
+        P (dict): The unigram probabilities.
+        Q (dict): The bigram probabilities.
+
+    Returns:
+        float: The energy of the permutation.
+    """
+    sigma_inv = inv_perm(perm)
+    
+    decoded_text = [sigma_inv.get(symbol, symbol) for symbol in scrambled_text]
+
+    epsilon = 1e-12
+    energy = -math.log(P.get(decoded_text[0], epsilon))
+
+    for j in range(len(decoded_text) - 1):
+        bigram = (decoded_text[j], decoded_text[j+1])
+        energy -= math.log(Q.get(bigram, epsilon))
+    
+    return energy
+
+def propose_permutation(perm):
+    """
+    Proposes a new permutation by swapping two random elements.
+
+    Parameters:
+        perm (dict): The current permutation.
+
+    Returns:
+        dict: The proposed permutation.
+    """
+    new_perm = perm.copy()
+    keys = list(new_perm.keys())
+    i, j = random.sample(keys, 2)
+    new_perm[i], new_perm[j] = new_perm[j], new_perm[i]
+
+    return new_perm
+
+def metropolis_step(perm, scrambled_text, P, Q, beta=1, old_energy=None):
+    """
+    Performs a single step of the Metropolis-Hastings algorithm.
+
+    Parameters:
+        perm (dict): The current permutation.
+        scrambled_text (str): The scrambled text.
+        P (dict): The unigram probabilities.
+        Q (dict): The bigram probabilities.
+        beta (float): The inverse temperature. Default is 1.
+
+    Returns:
+        dict: The new permutation.
+    """
+    new_perm = propose_permutation(perm)
+    
+    if old_energy is None:
+        old_energy = energy_function(perm, scrambled_text, P, Q)
+    
+    new_energy = energy_function(new_perm, scrambled_text, P, Q)
+
+    E_delta = new_energy - old_energy
+
+    if E_delta < 0 or random.random() < math.exp(-beta * E_delta):
+        return new_perm, new_energy
+    
+    return perm, old_energy
+
+def metropolis_hastings(scrambled_text, P, Q, beta=1, n=10**5, perm=None):
+    """
+    Runs the Metropolis-Hastings algorithm to decode a scrambled text.
+
+    Parameters:
+        scrambled_text (str): The scrambled text.
+        P (dict): The unigram probabilities.
+        Q (dict): The bigram probabilities.
+        beta (float): The inverse temperature. Default is 1.
+        n (int): The length of the chain to be returned. Default is 1e5.
+    
+    Returns:
+        list: The chain of permutations.
+    """
+    if perm is None:
+        alphabet = list("abcdefghijklmnopqrstuvwxyz ")
+        perm = {k: k for k in alphabet}
+
+    energy = math.inf
+    chain = []
+
+    for _ in tqdm(range(n+10**4)):
+        perm, energy = metropolis_step(perm, scrambled_text, P, Q, beta, energy)
+        chain.append(perm)
+
+    return chain[10**4:]
+
+def main():
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'pg2554.txt')
+
+    df1 = ngrams.load_ngrams(file_path, 'char', 1)
+    df2 = ngrams.load_ngrams(file_path, 'char', 2)
+
+    P = {row['1-gram']: row['frequency'] for _, row in df1.iterrows()}
+    Q = {tuple(row['2-gram']): row['frequency'] for _, row in df2.iterrows()}
+
+    alphabet = list("abcdefghijklmnopqrstuvwxyz ")
+    permutation = alphabet.copy()
+    random.shuffle(permutation)
+
+    sigma = {k: v for k, v in zip(alphabet, permutation)}
+
+    text = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way—in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only."
+    
+    extra_chars = "’‘“”"
+    remove_chars = string.punctuation + extra_chars
+    translator = str.maketrans('', '', remove_chars)
+    text = text.translate(translator)
+    text = text.lower()
+
+    scrambled_text = "".join([sigma.get(symbol, symbol) for symbol in text])
+
+    print(f"Scrambled text: {scrambled_text}")
+
+    chain = metropolis_hastings(scrambled_text, P, Q, beta=1, n=10**5)
+
+    count = Counter([tuple(perm.items()) for perm in chain])
+    perms = count.most_common(1)
+    
+    for perm, freq in perms:
+        perm_inv = inv_perm(dict(perm))
+        decoded_text = "".join([perm_inv.get(symbol, symbol) for symbol in scrambled_text])
+        print(f"Frequency: {freq/len(chain)}")
+        print(f"Decoded text: {decoded_text}")
+
+if __name__ == "__main__":
+    main()
---
+++ code/ngrams.py
@@ -0,0 +1,177 @@
+import os
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
+from nltk.util import ngrams
+from collections import Counter
+import string
+import pandas as pd
+import random
+from tqdm import tqdm
+
+def load_ngrams(file_path, ngram_type, n):
+    """
+    Load the n-grams from the file (Crime and Punishment).
+
+    Parameters:
+        file_path (str): The path to the file containing the text.
+        ngram_type (str): The type of n-grams to load. Must be 'char' or 'word'.
+        n (int): The size of the n-grams.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the n-grams and their counts.
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    text = remove_gutenberg_header_footer(text)
+    counts = get_ngrams(text, ngram_type, n)
+
+    df = pd.DataFrame(list(counts.items()), columns=[f'{n}-gram', 'count'])
+    df['frequency'] = df['count'] / df['count'].sum()
+    df = df.sort_values(by='count', ascending=False, ignore_index=True)
+
+    return df
+
+def remove_gutenberg_header_footer(text):
+    """
+    Remove the Project Gutenberg header and footer from the text (Crime and Punishment).
+
+    Parameters:
+        text (str): The text to process.
+
+    Returns:
+        str: The text without the Project Gutenberg header and footer.
+    """
+    start_marker = "*** START OF THE PROJECT GUTENBERG EBOOK CRIME AND PUNISHMENT ***"
+    end_marker = "*** END OF THE PROJECT GUTENBERG EBOOK CRIME AND PUNISHMENT ***"
+
+    start = text.find(start_marker)
+    end = text.find(end_marker)
+
+    if start != -1 and end != -1:
+        text = text[start + len(start_marker):end]
+
+    return text.strip()
+
+def get_ngrams(text, ngram_type, n):
+    """
+    Get the n-grams and counts from the text.
+
+    Parameters:
+        text (str): The text to process.
+        ngram_type (str): The type of n-grams to get. Must be 'char' or 'word'.
+        n (int): The size of the n-grams.
+    
+    Raises:
+        ValueError: If ngram_type is not 'word' or 'char'.    
+    
+    Returns:
+        Counter: A Counter object with the n-grams and their counts.
+    """
+    if ngram_type == 'word':
+        extra_chars = "’‘“”"
+        remove_chars = string.punctuation + extra_chars
+        translator = str.maketrans('', '', remove_chars)
+        text = text.translate(translator)
+        text = text.lower()
+        tokens = nltk.word_tokenize(text)
+        ngrams_list = list(ngrams(tokens, n))
+    elif ngram_type == 'char':
+        text = text.lower()
+        allowed_chars = set(string.ascii_lowercase + ' ')
+        filtered_text = "".join(ch for ch in text if ch in allowed_chars)
+        ngrams_list = [filtered_text[i:i+n] for i in range(len(filtered_text) - n + 1)]
+    else:
+        raise ValueError("ngram_type must be 'word' or 'char'")
+    
+    return Counter(ngrams_list)
+
+def generate_text(df, type, n, length):
+    """
+    Generate text using the n-grams from the DataFrame.
+
+    Parameters:
+        df (pd.DataFrame): The DataFrame containing the n-grams and their counts.
+        type (str): The type of n-grams. Must be 'word' or 'char'.
+        n (int): The size of the n-grams.
+        length (int): The length of the text to generate.
+    
+    Raises:
+        ValueError: If type is not 'word' or 'char'.    
+
+    Returns:
+        str: The generated text.
+    """
+    if type == 'word':
+        ngrams_list = df[f'{n}-gram'].tolist()
+        counts = df['count'].tolist()
+    elif type == 'char':
+        ngrams_list = df[f'{n}-gram'].tolist()
+        counts = df['count'].tolist()
+    else:
+        raise ValueError("type must be 'word' or 'char'")
+
+    if type == 'word':
+        if n == 1:
+            text_tokens = []
+            for _ in tqdm(range(length)):
+                next_ngram = random.choices(ngrams_list, weights=counts)[0]
+                text_tokens.append(next_ngram[0])
+            text = " ".join(text_tokens)
+        else:
+            text_tokens = list(random.choice(ngrams_list))
+            for _ in tqdm(range(length - n)):
+                prefix = tuple(text_tokens[-(n-1):])
+                next_ngrams = [ngram for ngram in ngrams_list if ngram[:n-1] == prefix]
+                if not next_ngrams:
+                    break
+                next_ngram = random.choices(next_ngrams, weights=[counts[ngrams_list.index(ngram)] for ngram in next_ngrams])[0]
+                text_tokens.append(next_ngram[-1])
+            text = " ".join(text_tokens)
+    else:
+        if n == 1:
+            text = ""
+            for _ in tqdm(range(length)):
+                next_ngram = random.choices(ngrams_list, weights=counts)[0]
+                text += next_ngram[0]
+        else:
+            text = random.choice(ngrams_list)
+            for _ in tqdm(range(length - n)):
+                next_ngrams = [ngram for ngram in ngrams_list if ngram.startswith(text[-(n-1):])]
+                if not next_ngrams:
+                    break
+                next_ngram = random.choices(next_ngrams, weights=[counts[ngrams_list.index(ngram)] for ngram in next_ngrams])[0]
+                text += next_ngram[-1]
+
+    return text
+
+def main():
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'pg2554.txt')
+
+    char_onegram_counts = load_ngrams(file_path, 'char', 1)
+    char_bigram_counts = load_ngrams(file_path, 'char', 2)
+
+    print("Top 10 most common character 1-grams:")
+    print(char_onegram_counts.head(10))
+    print("\nTop 10 most common character 2-grams:")
+    print(char_bigram_counts.head(10))
+
+    word_onegram_counts = load_ngrams(file_path, 'word', 1)
+    word_bigram_counts = load_ngrams(file_path, 'word', 2)
+
+    print("\nTop 10 most common word 1-grams:")
+    print(word_onegram_counts.head(10))
+    print("\nTop 10 most common word 2-grams:")
+    print(word_bigram_counts.head(10))
+
+    generated_text = generate_text(char_onegram_counts, 'char', 2, 100)
+    print("\nGenerated text:")
+    print(generated_text)
+
+    generated_text = generate_text(word_onegram_counts, 'word', 2, 10)
+    print("\nGenerated text:")
+    print(generated_text)
+
+if __name__ == "__main__":
+    main()
